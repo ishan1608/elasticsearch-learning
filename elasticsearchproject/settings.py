0e3ab4767e26d779a0078c8c4fca9d2791a561e1
Ishan <ishanatmuzaffarpur@gmail.com>
2017-09-29 17:33:58 +0530

    Added Postgres database

--- elasticsearchproject/settings.py
+++ elasticsearchproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'elasticsearchproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'elastic-search',
+        'USER': 'elastic-search',
+        'PASSWORD': 'elastic-search',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
