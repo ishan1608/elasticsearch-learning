ff13db9f4065ad1d55cefd93f15fe3eed7edff52
Ishan <ishanatmuzaffarpur@gmail.com>
2017-10-01 10:01:05 +0530

    Added Blog as a ForeignKey

--- elasticsearchapp/models.py
+++ elasticsearchapp/models.py
@@ -6,15 +6,24 @@ from django.db import models
 from django.utils import timezone
 
 
-# Blogpost to be indexed into ElasticSearch
+class Blog(models.Model):
+    subdomain = models.CharField(max_length=16)
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return 'Blog: {}-{}'.format(self.subdomain, self.name).encode('utf-8')
+
+
+# Blogpost to be indexed based on blog into ElasticSearch
 class BlogPost(models.Model):
+    blog = models.ForeignKey(Blog)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blogpost')
     posted_date = models.DateField(default=timezone.now)
     title = models.CharField(max_length=200)
     text = models.TextField(max_length=1000)
 
     def __str__(self):
-        return 'Blog: {}'.format(self.title).encode('utf-8')
+        return 'Post: {}'.format(self.title).encode('utf-8')
 
     def indexing(self):
         from .search import BlogPostIndex
