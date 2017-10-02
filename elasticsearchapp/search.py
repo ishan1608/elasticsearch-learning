b88f2c76d8ae9ac7b98dc6d7956a7302d1342d3b
Ishan <ishanatmuzaffarpur@gmail.com>
2017-10-02 11:17:26 +0530

    Added Search Filtering

--- elasticsearchapp/search.py
+++ elasticsearchapp/search.py
@@ -2,7 +2,7 @@ from elasticsearch.helpers import bulk
 from elasticsearch_dsl import DocType, Text, Date, Search, Object
 from elasticsearch_dsl.connections import connections
 
-from . import models
+from .models import Blog, BlogPost
 
 es = connections.create_connection(
     hosts=['localhost'],
@@ -25,9 +25,34 @@ class BlogPostIndex(DocType):
 
 def bulk_indexing():
     BlogPostIndex.init()
-    bulk(client=es, actions=(b.indexing() for b in models.BlogPost.objects.all().iterator()))
+    bulk(client=es, actions=(b.indexing() for b in BlogPost.objects.all().iterator()))
+
+
+def _print_results(result):
+    for hit in result.hits:
+        print hit.blog, hit.title
+
+
+def _search_blog(subdomain, search_term):
+    blog = Blog.objects.get(subdomain=subdomain)
+    s = Search().query('match', blog=blog.subdomain).filter('term', text=search_term)
+    return s.execute()
 
 
 def search(text):
     s = Search().filter('term', text=text)
-    return s.execute()
+    result = s.execute()
+    _print_results(result)
+    return result
+
+
+def search_umbra(text):
+    result = _search_blog('umbra3d', text)
+    _print_results(result)
+    return result
+
+
+def search_postman(text):
+    result = _search_blog('postman', text)
+    _print_results(result)
+    return result
