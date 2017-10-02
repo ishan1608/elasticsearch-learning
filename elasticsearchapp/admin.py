162e37dc4a7e59cce9217212bb67d28985809da4
Ishan <ishanatmuzaffarpur@gmail.com>
2017-10-02 10:21:25 +0530

    Added Blog to the admin panel

--- elasticsearchapp/admin.py
+++ elasticsearchapp/admin.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import BlogPost
+from .models import BlogPost, Blog
 
-# Need to register my BlogPost so it shows up in the admin
+
 admin.site.register(BlogPost)
+admin.site.register(Blog)
